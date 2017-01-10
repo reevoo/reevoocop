d0d9e850f50c8d0a8dfda675a85c16cc8673eba0
Ed Robinson <ed.robinson@reevoo.com>
2017-01-10 20:56:37 +0000

    Support rake 11+

--- lib/reevoocop/rake_task.rb
+++ lib/reevoocop/rake_task.rb
@@ -4,6 +4,21 @@ require "rubocop/rake_task"
 
 module ReevooCop
   class RakeTask < RuboCop::RakeTask
+    def initialize(*args, &task_block) # rubocop:disable Metrics/AbcSize
+      setup_ivars(args)
+
+      desc "Run RuboCop" unless ::Rake.application.last_description
+
+      task(name, *args) do |_, task_args|
+        RakeFileUtils.send(:verbose, verbose) do
+          yield(*[self, task_args].slice(0, task_block.arity)) if block_given?
+          run_main_task(verbose)
+        end
+      end
+
+      setup_subtasks(name, *args, &task_block)
+    end
+
     def run_cli(verbose, options)
       require "reevoocop"
       super(verbose, options)
